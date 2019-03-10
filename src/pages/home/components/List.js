b3ce00971bbbb932a8fcf8054403c7c1dea44d47
qfstudy <qifeistudy@gmail.com>
2019-03-10 23:59:06 +0800

    文章标题以及简介

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react'
 import {ListItem,ListInfo} from '../style'
+import {connect} from 'react-redux'
 
 class List extends Component{
   render(){
+    const {list} = this.props
     return (
-      <ListItem>
-        <img src="https://upload-images.jianshu.io/upload_images/10015224-4f930b6ec4481450.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240" alt=""/>
-        <ListInfo></ListInfo>
-      </ListItem>
+      <div>
+        {
+          list.map((item)=>{
+            return(
+              <ListItem key={item.get('id')}>
+                <img className="pic" alt="" src={item.get('imgUrl')}/>
+                <ListInfo>
+                  <h3 className="title">{item.get('title')}</h3>
+                  <p className="desc">
+                    {item.get('desc')}
+                  </p>
+                </ListInfo>
+              </ListItem>
+            )
+          })
+        }
+      </div>      
     )
   }
 }
 
-export default List
+const mapState=(state)=>({
+  list: state.getIn(['home','articleList'])
+})
+
+export default connect(mapState)(List)
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -10,6 +10,23 @@ const defaultState=fromJS({
     id: 2,
     title: '社会热点2',
     imgUrl: '//upload.jianshu.io/collections/images/261938/man-hands-reading-boy-large.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64'
+  }],
+  articleList:[{
+    id: 1,
+    title: '写作，也许是为了对抗孤独（上）',
+    desc: '在简书平台上，发现一个有趣的现象，简友们每隔一段时间，都会写文提醒自己，不要在乎赞与钻，那些都是浮云，如果跟风跑，就是浪费时间。 可是每隔一段时.',
+    imgUrl: 'https://upload-images.jianshu.io/upload_images/10015224-4f930b6ec4481450.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+  },
+  {
+    id: 2,
+    title: '写作，也许是为了对抗孤独（2）',
+    desc: '在简书平台上，发现一个有趣的现象，简友们每隔一段时间，都会写文提醒自己，不要在乎赞与钻，那些都是浮云，如果跟风跑，就是浪费时间。 可是每隔一段时.',
+    imgUrl: 'https://upload-images.jianshu.io/upload_images/10015224-4f930b6ec4481450.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+  },{
+    id: 3,
+    title: '写作，也许是为了对抗孤独（3）',
+    desc: '在简书平台上，发现一个有趣的现象，简友们每隔一段时间，都会写文提醒自己，不要在乎赞与钻，那些都是浮云，如果跟风跑，就是浪费时间。 可是每隔一段时.',
+    imgUrl: 'https://upload-images.jianshu.io/upload_images/10015224-4f930b6ec4481450.jpeg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
   }]
 })
 
