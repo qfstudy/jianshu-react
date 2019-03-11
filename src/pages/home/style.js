3df40dc2ea974df1cd7d824232279832972b4093
qfstudy <qifeistudy@gmail.com>
2019-03-11 23:26:38 +0800

    添加回到顶部功能

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -80,6 +80,18 @@ export const ListInfo=styled.div`
   }
 `
 
+export const LoadMore=styled.div`
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  margin: 30px 0;
+  text-align: center;
+  background:#a5a5a5;
+  border-radius: 20px;
+  color: #fff;
+  cursor: pointer;
+`
+
 // RecommentWrapper style
 
 export const RecommentWrapper=styled.div`
@@ -105,8 +117,14 @@ export const WriterWrapper=styled.div`
   text-align: center;
 `
 
-
-
-
-
-
+export const BackTop=styled.div`
+  position: fixed;
+  right: 100px;
+  bottom: 100px;
+  width: 60px;
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+  border: 1px solid #ccc;
+  font-size: 12px;
+`
