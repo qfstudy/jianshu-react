5b0fae5ac01c9b13c68c892f0f3505a3f3d284c6
qfstudy <qifeistudy@gmail.com>
2019-03-12 22:13:13 +0800

    详情页布局以及获取详情页数据

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {Detailwrapper,Header,Content} from './style'
+import {actionCreators} from './store'
 
 class Detail extends Component{
   render(){
     return (
-      <div>Detail</div>
+      <Detailwrapper>
+        <Header>{this.props.title}</Header>
+        <Content 
+          dangerouslySetInnerHTML={{__html:this.props.content}} 
+        />
+      </Detailwrapper>
     )
   }
+  componentDidMount(){
+    this.props.getDetail()
+  }
 }
+const mapState=(state)=>({
+  title: state.getIn(['detail','title']),
+  content: state.getIn(['detail','content'])
+})
 
-export default Detail
+const mapDispatch=(dispatch)=>({
+  getDetail(){
+    dispatch(actionCreators.getDetail())
+  }
+})
+
+export default connect(mapState,mapDispatch)(Detail)
