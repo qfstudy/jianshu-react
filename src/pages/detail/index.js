333fe3d56f58c61251adb39f8242142bf6f29547
qfstudy <qifeistudy@gmail.com>
2019-03-12 23:51:22 +0800

    登录页面布局

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react'
+import React, {PureComponent} from 'react'
 import {connect} from 'react-redux'
 import {Detailwrapper,Header,Content} from './style'
 import {actionCreators} from './store'
 
-class Detail extends Component{
+class Detail extends PureComponent{
   render(){
     return (
       <Detailwrapper>
